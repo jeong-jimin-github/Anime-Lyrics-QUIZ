0a1bb2560c25c992f620ff8206dae4f224742ef1
Jeong Jimin <95138574+jeong-jimin-github@users.noreply.github.com>
2022-10-29 18:16:40 +0900

    Update updater.py

--- tools/updater.py
+++ tools/updater.py
@@ -4,5 +4,5 @@ def write_df_to_local(df, file_path):
     df.to_csv(file_path, index=False)
 sheet_url = 'https://docs.google.com/spreadsheets/d/1O0qIhEPa2TJNwaU5MVk9NjwH7FOx8wrqouf_KA5AMgY/export?format=csv'
 df = pd.read_csv(sheet_url)
-file_path = '//wsl.localhost/Ubuntu-20.04/home/jm/anime/data.csv'
+file_path = 'data.csv'
 write_df_to_local(df, file_path)
